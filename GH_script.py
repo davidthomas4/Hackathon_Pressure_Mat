4593656736828939db311f6d87a5eab320af5154
David Thomas <david.thomas95@hotmail.co.uk>
2019-11-17 02:26:27 +0000

    Added "Normalisation".

--- GH_script.py
+++ GH_script.py
@@ -54,6 +54,8 @@ heatmap.axes.set_ylim(0,0.75)
 # plot.                           #
 ###################################
 
+first_call=True
+initial_data=np.zeros(16)
 k = 0 # frame counter
 ser = serial.Serial('/dev/ttyACM0',9600)
 s = [0 for i in range(1,17)]
@@ -62,17 +64,21 @@ while True:
     if k == 1000:
      break
     tot = np.zeros(16)
-    for i in range(1,5):
+    for i in range(1,10):
         s  = str(ser.readline())
         num = re.findall(r'\d+', s)
         datain = list(map(int, num))
         if(len(datain)!=16):
              continue
+        if(first_call):
+             first_call=False
+             print("first call.")
+             initial_data=np.array(datain, dtype=float)
+             continue
         tot += np.array(datain, dtype=float)
-    data=tot/5
-    maxtot=max(data)
+    data=tot/10-initial_data
     cc = griddata((xs, ys), data, (xx[None, :], yy[:, None]), method='cubic')
-    heatmap.contourf(xx, yy, cc, 300, cmap=cmap_choice, vmin=0.0, vmax=1000)
+    heatmap.contourf(xx, yy, cc, 300, cmap=cmap_choice, vmin=-500, vmax=500)
 
     Bottom = [data[0], data[4], data[8], data[12]]
     Top = [data[3], data[7], data[11], data[15]]
@@ -97,15 +103,15 @@ while True:
     else:
         txtv = "Looking good (forward/backward)"
 
-    HCD = heatmap.contourf(xx, yy, cc, 300, cmap=cmap_choice, vmin=0.0,
-                           vmax=1000)
-    heatmap.axes.tick_params(width=2, length=10, direction='in', top=True,
-                             bottom=False)
-    cb_ax = fig.add_axes([0.87,0.15,0.02,0.75])
-    cbar = fig.colorbar(HCD, cax=cb_ax, ticks=[0, 250, 500, 750, 1000])
+#    HCD = heatmap.contourf(xx, yy, cc, 300, cmap=cmap_choice, vmin=0.0,
+#                           vmax=1000)
+#    heatmap.axes.tick_params(width=2, length=10, direction='in', top=True,
+#                             bottom=False)
+#    cb_ax = fig.add_axes([0.87,0.15,0.02,0.75])
+#    cbar = fig.colorbar(HCD, cax=cb_ax, ticks=[0, 250, 500, 750, 1000])
 #    cbar.set_clim(0,1000)
 #    cbar.ax_set_yticklabels([0,0.25,0.5,0.75,1])
-    cbar.ax.tick_params(width=2, length=10)
+#    cbar.ax.tick_params(width=2, length=10)
 
     for txt in fig.texts:
         txt.set_visible(False)
